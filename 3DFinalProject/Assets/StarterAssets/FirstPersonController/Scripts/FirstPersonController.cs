0e2c0dc248b309f42f8d550292b88f44a8b9b953
benny999 <benny999.yu@gmail.com>
2024-12-03 00:32:46 +0800

    [create] create a new scene call initScene and implement basic player functions

--- 3DFinalProject/Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
+++ 3DFinalProject/Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
@@ -15,7 +15,7 @@ namespace StarterAssets
 		[Tooltip("Move speed of the character in m/s")]
 		public float MoveSpeed = 4.0f;
 		[Tooltip("Sprint speed of the character in m/s")]
-		public float SprintSpeed = 6.0f;
+		public float WalkSpeed = 6.0f;
 		[Tooltip("Rotation speed of the character")]
 		public float RotationSpeed = 1.0f;
 		[Tooltip("Acceleration and deceleration")]
@@ -154,7 +154,7 @@ namespace StarterAssets
 		private void Move()
 		{
 			// set target speed based on move speed, sprint speed and if sprint is pressed
-			float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
+			float targetSpeed = _input.sprint ? WalkSpeed : MoveSpeed;
 
 			// a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
 
