e131b967b84ec6135e29e93e683fd97c9b767bfd
touhouyajin <126633735+touhouyajin@users.noreply.github.com>
2024-12-27 19:30:29 +0800

    [Feature] Add player audio source and some simple clip for player behavior

--- 3DFinalProject/Assets/Scripts/Player/Audio/PlayerAudioController.cs
+++ 3DFinalProject/Assets/Scripts/Player/Audio/PlayerAudioController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerAudioController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // AudioSource component to play sounds
+    [SerializeField]
+    private AudioSource audioSource = default;
+
+    // Audio clips for corpse flipping and relaxing breathing
+    [SerializeField]
+    private AudioClip corpseFlipClip; // Clip for corpse flipping sound
+    [SerializeField]
+    private AudioClip relaxingBreathClip; // Clip for relaxing breath sound
+
     void Start()
     {
-        
+        // Ensure AudioSource is assigned
+        audioSource = GetComponent<AudioSource>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    // Play corpse flip sound
+    public void PlayCorpseFlip()
+    {
+        if (corpseFlipClip != null)
+        {
+            audioSource.PlayOneShot(corpseFlipClip);
+        }
+    }
+
+    // Play relaxing breath sound
+    public void PlayRelaxingBreath()
+    {
+        if (relaxingBreathClip != null)
+        {
+            audioSource.PlayOneShot(relaxingBreathClip);
+        }
+    }
+    
 }
