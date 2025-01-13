f4d0e3b7b27122ea5a9e3fb0bb2632160684ccf4
葉宇瀚 <bowsoul062904@gmail.com>
2025-01-13 20:24:34 +0800

    Fix : Fix dead body in the ground

--- 3DFinalProject/Assets/Scripts/Game/ObjectDistributer.cs
+++ 3DFinalProject/Assets/Scripts/Game/ObjectDistributer.cs
@@ -17,6 +17,8 @@ public class ObjectDistributer : MonoBehaviour
     private Transform Player;
     [SerializeField]
     private GameObject GM;
+    [SerializeField]
+    private Terrain Terrain;
 
     [Header("Item Prefabs")]
     [SerializeField]
@@ -233,7 +235,29 @@ public class ObjectDistributer : MonoBehaviour
 
     private void DistributeDeadbody()
     {
-        SpawnOutsideItems(DeadbodyPrefabs[correctDeadbodyID - 1], 1);
+        //SpawnOutsideItems(DeadbodyPrefabs[correctDeadbodyID - 1], 1);
+        Instantiate(DeadbodyPrefabs[correctDeadbodyID - 1], GetRandomTreePosition(), transform.rotation);
+    }
+
+    private Vector3 GetRandomTreePosition()
+    {
+        // 獲取地形的樹實例數據
+        TreeInstance[] trees = Terrain.terrainData.treeInstances;
+        if (trees.Length == 0)
+        {
+            Debug.LogWarning("Terrain 上沒有樹！");
+            return Vector3.zero;
+        }
+
+        // 隨機選擇一棵樹
+        int randomIndex = Random.Range(0, trees.Length);
+        TreeInstance randomTree = trees[randomIndex];
+
+        // 計算樹的世界座標
+        Vector3 treeWorldPosition = Vector3.Scale(randomTree.position, Terrain.terrainData.size);
+        treeWorldPosition += Terrain.transform.position;
+        treeWorldPosition = new Vector3(treeWorldPosition.x + 2, treeWorldPosition.y + 3, treeWorldPosition.z);
+        return treeWorldPosition;
     }
 
     private void SpawnOutsideItems(GameObject _prefab, int _prefabNum)
